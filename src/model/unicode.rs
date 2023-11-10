493c733f5b7b05eff6aa8b2785d08b4dcfcb0e01
Francis Russell <francis@unchartedbackwaters.co.uk>
2023-11-10 17:22:21 +0000

    Remove duplicated Unicode conversion code.

--- src/model/unicode.rs
+++ src/model/unicode.rs
@@ -1,9 +1,5 @@
 /// Use to `to_unicode` function for String
 pub trait Unicode {
-    fn to_unicode(&self) -> Vec<u8>;
-}
-
-impl Unicode for String {
     /// Convert any string into utf-16le string
     ///
     /// # Example
@@ -12,5 +8,13 @@ impl Unicode for String {
     /// let s = "foo".to_string();
     /// assert_eq!(s.to_unicode(), [102, 0, 111, 0, 111, 0])
     /// ```
+    fn to_unicode(&self) -> Vec<u8>;
+}
+
+impl Unicode for &str {
     fn to_unicode(&self) -> Vec<u8> { self.encode_utf16().flat_map(u16::to_le_bytes).collect() }
+}
+
+impl Unicode for String {
+    fn to_unicode(&self) -> Vec<u8> { self.as_str().to_unicode() }
 }
